2e269f1d8204d334f29eef4423b319b84e42b5af
dgerena <dgerena@fullsail.edu>
2014-06-05 09:41:07 -0400

    notes +
    more notes

--- Classwork/day2/day1.php
+++ Classwork/day2/day1.php
@@ -1,18 +1,30 @@
-<!doctype>
-<html>
-    <head>
-        <title>Notes for class</title>
-    </head>
-    <body>
-        <form action="" method="post">
-            <input type="text">
-            <button type="submit">Submit</button>
-        </form>
-    </body>
-</html>
+<? $name = "eli"
+<form action="?" method="post">
+<input type="text" name="name">
+<input type="text" name="lastname">
+<input type="submit"/>
+</form>
 
-<?php
 
+<!--<?php
 echo:tst
+//Other processes use these to print to screen
+   /* alert
+    print
+    trace
+    console
+    /
+//methods of pushing to the screen
+    
+    echo
+    print
+    var_dump();//Most usefull in real life work
 
-?>
+//Loops
+
+foreach($_POST as x){
+    echo "<span style='color:red'>".$x."</span><br/>";
+}
+
+var_dump(post)
+?>-->
